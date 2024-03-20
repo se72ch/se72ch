9c373f05106cb8502dcc77b497ae8793734a527e
Search829 <134131449+Search829@users.noreply.github.com>
2024-03-20 15:32:26 +0300

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# تثبيت theHarvester
+echo -e "\e[1;34mInstalling theHarvester...\e[0m"
+sudo apt-get install theHarvester -y
+echo -e "\e[1;32mtheHarvester installed successfully.\e[0m"
+
+# تثبيت figlet
+echo -e "\e[1;34mInstalling figlet...\e[0m"
+sudo apt-get install figlet -y
+echo -e "\e[1;32mfiglet installed successfully.\e[0m"
+
+# تثبيت dirsearch
+echo -e "\e[1;34mInstalling dirsearch...\e[0m"
+sudo apt-get install dirsearch -y
+echo -e "\e[1;32mdirsearch installed successfully.\e[0m"
+
+# تثبيت curl
+echo -e "\e[1;34mInstalling curl...\e[0m"
+sudo apt install curl -y
+echo -e "\e[1;32mcurl installed successfully.\e[0m"
+
+# تثبيت jq
+echo -e "\e[1;34mInstalling jq...\e[0m"
+sudo apt-get install jq -y
+echo -e "\e[1;32mjq installed successfully.\e[0m"
+
+# تثبيت httprobe
+echo -e "\e[1;34mInstalling httprobe...\e[0m"
+sudo apt install httprobe -y
+echo -e "\e[1;32mhttprobe installed successfully.\e[0m"
+
+echo -e "\e[1;32mInstallation completed successfully.\e[0m"
